c1aca12e71a184149e39a631e23e24201eb76992
franco romano <franromano99@outlook.com>
2022-07-20 12:29:57 -0300

    Fr - listar clientes

---
+++ Ecomerce_back/Controllers/ClientesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Ecomerce_back.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientesController : Controller
+    {
+        public static string ConnectionString = "data source=localhost; Initial Catalog=ecommerce; Integrated Security=True";
+
+
+        [HttpGet(Name = "ListarClientes")]
+        public async Task<string> ListarClientes()
+        {
+            DataSet ds = new DataSet();
+            string response;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection())
+                {
+                    connection.ConnectionString = ConnectionString;
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        connection.Open();
+                        command.CommandText = @"select * from clientes";
+                        command.ExecuteNonQuery();
+
+                        SqlDataAdapter da = new SqlDataAdapter(command);
+
+                        using (da)
+                        {
+                            da.Fill(ds);
+                        }     
+                    }
+               
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            // Parse ds to Json
+            response = ds.GetXml();
+            var doc = XDocument.Parse(response);
+            return JsonConvert.SerializeXNode(doc, Newtonsoft.Json.Formatting.None, omitRootObject: true);
+        }
+
+    }
+}
---
+++ Ecomerce_back/interfaces/IClientes.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecomerce_back.interfaces
+{
+    public interface IClientes
+    {
+        int Id { get; set; }
+        string email { get; set; }
+        string nombre { get; set; }
+        string apellido { get; set; }
+        string telefono { get; set; }
+        string direccion { get; set; }
+        string contraseña { get; set; }
+    }
+}
